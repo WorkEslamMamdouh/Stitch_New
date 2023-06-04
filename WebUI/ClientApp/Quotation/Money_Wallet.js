20bddeab07a1d37b025f9d8f6b72e949b4fb6bf9
WorkEslamMamdouh <workeslammamdouh619@outlook.com>
2023-06-04 23:02:24 +0300

    Update

--- WebUI/ClientApp/Quotation/Money_Wallet.js
+++ WebUI/ClientApp/Quotation/Money_Wallet.js
@@ -6,25 +6,30 @@ var Money_Wallet;
     var AllDisplay = new Array();
     var Display = new Array();
     var Model = new DataAll();
+    var JGrid = new JsGrid();
     var btnExchange;
     var btnReceipt;
     var a_Expans;
     var a_Resive;
     var a_View;
-    var Glopl_Type = '1';
+    var txtSearch;
+    var Glopl_Type = 'Exchange';
     function InitalizeComponent() {
         Tabs_click();
-        $('#txtdate').val(GetDate());
+        InitializeGrid();
+        txtSearch = document.getElementById("txtSearch");
         btnExchange = document.getElementById("btnExchange");
         btnReceipt = document.getElementById("btnReceipt");
         a_Expans = document.getElementById("a_Expans");
         a_Resive = document.getElementById("a_Resive");
+        a_View = document.getElementById("a_View");
         btnExchange.onclick = function () { AppTans(Glopl_Type); };
         btnReceipt.onclick = function () { AppTans(Glopl_Type); };
-        a_Expans.onclick = function () { Glopl_Type = '1'; };
-        a_Resive.onclick = function () { Glopl_Type = '0'; };
-        //a_View.onclick = () => { };
-        Clean();
+        a_Expans.onclick = function () { $('#Rec_Exch_Tab').removeClass('display_none'); $('#Views_Tab').addClass('display_none'); Glopl_Type = 'Exchange'; };
+        a_Resive.onclick = function () { $('#Rec_Exch_Tab').removeClass('display_none'); $('#Views_Tab').addClass('display_none'); Glopl_Type = 'Receipt'; };
+        a_View.onclick = function () { $('#Views_Tab').removeClass('display_none'); $('#Rec_Exch_Tab').addClass('display_none'); };
+        txtSearch.onkeyup = txtSearch_change;
+        DisplayAll();
     }
     Money_Wallet.InitalizeComponent = InitalizeComponent;
     function Tabs_click() {
@@ -38,6 +43,79 @@ var Money_Wallet;
             }
         });
     }
+    function InitializeGrid() {
+        JGrid.ElementName = "JGrid";
+        JGrid.PrimaryKey = "ID";
+        JGrid.Paging = true;
+        JGrid.PageSize = 10;
+        JGrid.Sorting = true;
+        JGrid.InsertionMode = JsGridInsertionMode.Binding;
+        JGrid.Editing = false;
+        JGrid.Inserting = false;
+        JGrid.SelectedIndex = 1;
+        JGrid.OnItemEditing = function () { };
+        JGrid.Columns = [
+            { title: "ID", name: "ID", type: "text", width: " ", visible: false },
+            { title: "TrNo", name: "ID", type: "text" },
+            { title: "TrDate", name: "TrDate", type: "text" },
+            { title: "Type", name: "Type", type: "text" },
+            { title: "Title", name: "Title", type: "text" },
+            {
+                title: "Delete",
+                itemTemplate: function (s, item) {
+                    var txt = document.createElement("input");
+                    txt.type = "button";
+                    txt.value = ("Delete");
+                    txt.id = "butDelete" + item.ID;
+                    txt.className = "btn btn-custon-four btn-danger ";
+                    txt.onclick = function (e) {
+                        Delete(item.ID);
+                    };
+                    return txt;
+                }
+            },
+        ];
+        //JGrid.Bind();
+    }
+    function txtSearch_change() {
+        $("#JGrid").jsGrid("option", "pageIndex", 1);
+        if (txtSearch.value != "") {
+            var search_1 = txtSearch.value.toLowerCase();
+            var SearchDetails = Display.filter(function (x) { return x.ID.toString().search(search_1) >= 0 || x.Title.toLowerCase().search(search_1) >= 0
+                || x.Type.toLowerCase().search(search_1) >= 0 || x.Remars.toLowerCase().search(search_1) >= 0; });
+            JGrid.DataSource = SearchDetails;
+            JGrid.Bind();
+        }
+        else {
+            JGrid.DataSource = Display;
+            JGrid.Bind();
+        }
+    }
+    function DisplayAll() {
+        Ajax.CallAsync({
+            url: Url.Action("Get_Data", "Profile"),
+            data: { Name_txt: "Catch_Receipt" },
+            success: function (d) {
+                var result = JSON.parse(d);
+                var res = result;
+                Display_Grid(res);
+            }
+        });
+    }
+    function Display_Grid(_Display) {
+        debugger;
+        AllDisplay = _Display;
+        AllDisplay = AllDisplay.sort(dynamicSortNew("ID"));
+        Display = _Display;
+        //if (dbTypeF.value != "All") {
+        //    Display = Display.filter(x => x.Type == dbTypeF.value);
+        //}
+        //Display = Display.filter(x => x.TrDate >= txtDateFrom.value && x.TrDate <= txtDateTo.value);
+        Display = Display.sort(dynamicSortNew("ID"));
+        JGrid.DataSource = Display;
+        JGrid.Bind();
+        Clean();
+    }
     function AppTans(Type) {
         debugger;
         Model = new DataAll();
@@ -45,8 +123,8 @@ var Money_Wallet;
         Model.TrDate = DateFormatRep($('#txtdate').val());
         Model.Type = $('#TypeSours').val();
         Model.Title = Type;
-        //Model.ID = Number($('#txtTrNo').val());
-        Model.ID = 666;
+        Model.ID = Number($('#txtTrNo').val());
+        //Model.ID = 666;
         var Data = new Send_Data();
         Data.ID = Number($('#txtTrNo').val());
         Data.Name_Txt_Master = "Catch_Receipt";
@@ -61,23 +139,49 @@ var Money_Wallet;
             data: { Data: JSON.stringify(Data) },
             success: function (d) {
                 var result = JSON.parse(d);
-                Clean();
                 var res = result;
-                //Display_Grid(res)
-                //JGrid.SelectedItem = Model;
-                //GridDoubleClick();
+                Display_Grid(res);
+                Clean();
+            }
+        });
+    }
+    function Delete(ID) {
+        JGrid.SelectedItem = Display.filter(function (x) { return x.ID == ID; })[0];
+        Model = new DataAll();
+        Model.ID = ID;
+        var Data = new Send_Data();
+        Data.ID = Number($('#txtTrNo').val());
+        Data.Name_Txt_Master = "Catch_Receipt";
+        Data.Model = JSON.stringify(Model);
+        Data.StatusFlag = 'd';
+        debugger;
+        $.ajax({
+            url: Url.Action("Update_Data", "Profile"),
+            type: "POST",
+            dataType: 'json',
+            async: false,
+            data: { Data: JSON.stringify(Data) },
+            success: function (d) {
+                var result = JSON.parse(d);
+                var res = result;
+                Display_Grid(res);
+                Clean();
             }
         });
     }
     function Clean() {
-        $('#TypeSours').val('0');
+        debugger;
+        var MaxID = AllDisplay[0].ID;
+        $('#txtTrNo').val(MaxID + 1);
+        $('#TrNoLab').html('TrNo ( ' + MaxID + 1 + ' )');
+        $('#TypeSours').val('Cash');
         $('#txtRemark').val('');
         $('#txtdate').val(GetDate());
         $('#txtAmount').val('');
         setTimeout(function () { $('#txtRemark').focus(); }, 150);
         $('#btnReceipt').addClass('display_none');
         $('#btnExchange').addClass('display_none');
-        if (Glopl_Type == '1') {
+        if (Glopl_Type == 'Exchange') {
             $('#btnExchange').removeClass('display_none');
         }
         else {
--- WebUI/ClientApp/Quotation/Money_Wallet.ts
+++ WebUI/ClientApp/Quotation/Money_Wallet.ts
@@ -8,32 +8,38 @@ namespace Money_Wallet {
     var AllDisplay: Array<DataAll> = new Array<DataAll>();
     var Display: Array<DataAll> = new Array<DataAll>();
     var Model: DataAll = new DataAll();
+    var JGrid: JsGrid = new JsGrid();
 
     var btnExchange: HTMLButtonElement;
     var btnReceipt: HTMLButtonElement;
     var a_Expans: HTMLButtonElement;
     var a_Resive: HTMLButtonElement;
     var a_View: HTMLButtonElement;
+    var txtSearch: HTMLInputElement;
 
-    var Glopl_Type = '1';
+    var Glopl_Type = 'Exchange';
 
-    export function InitalizeComponent() { 
+    export function InitalizeComponent() {
+
         Tabs_click();
-        $('#txtdate').val(GetDate());
+        InitializeGrid(); 
 
+        txtSearch = document.getElementById("txtSearch") as HTMLInputElement;
         btnExchange = document.getElementById("btnExchange") as HTMLButtonElement;
         btnReceipt = document.getElementById("btnReceipt") as HTMLButtonElement;
         a_Expans = document.getElementById("a_Expans") as HTMLButtonElement;
         a_Resive = document.getElementById("a_Resive") as HTMLButtonElement;
+        a_View = document.getElementById("a_View") as HTMLButtonElement;
 
         btnExchange.onclick = () => { AppTans(Glopl_Type) };
         btnReceipt.onclick = () => { AppTans(Glopl_Type) };
-        a_Expans.onclick = () => { Glopl_Type = '1'; };
-        a_Resive.onclick = () => { Glopl_Type = '0'; };
-        //a_View.onclick = () => { };
+        a_Expans.onclick = () => { $('#Rec_Exch_Tab').removeClass('display_none'); $('#Views_Tab').addClass('display_none'); Glopl_Type = 'Exchange'; };
+        a_Resive.onclick = () => { $('#Rec_Exch_Tab').removeClass('display_none'); $('#Views_Tab').addClass('display_none'); Glopl_Type = 'Receipt'; };
+        a_View.onclick = () => { $('#Views_Tab').removeClass('display_none'); $('#Rec_Exch_Tab').addClass('display_none'); };
 
-        Clean();
+        txtSearch.onkeyup = txtSearch_change;
 
+        DisplayAll();
     } 
     function Tabs_click() {
 
@@ -56,8 +62,103 @@ namespace Money_Wallet {
 
         });
     }  
+    function InitializeGrid() {
+        JGrid.ElementName = "JGrid";
+        JGrid.PrimaryKey = "ID"; 
+        JGrid.Paging = true;
+        JGrid.PageSize = 10;
+        JGrid.Sorting = true;
+        JGrid.InsertionMode = JsGridInsertionMode.Binding;
+        JGrid.Editing = false;
+        JGrid.Inserting = false;
+        JGrid.SelectedIndex = 1;
+        JGrid.OnItemEditing = () => { };
+        JGrid.Columns = [
+            { title: "ID", name: "ID", type: "text", width: " ", visible: false },
+            { title: "TrNo", name: "ID", type: "text" },
+            { title: "TrDate", name: "TrDate", type: "text" },
+            { title: "Type", name: "Type", type: "text" },
+            { title: "Title", name: "Title", type: "text" },
+            {
+                title: "Delete",
+                itemTemplate: (s: string, item: DataAll): HTMLInputElement => {
+                    let txt: HTMLInputElement = document.createElement("input");
+                    txt.type = "button";
+                    txt.value = ("Delete");
+                    txt.id = "butDelete" + item.ID;
+                    txt.className = "btn btn-custon-four btn-danger ";
 
-   
+                    txt.onclick = (e) => {
+                        Delete(item.ID);
+                    };
+                    return txt;
+                }
+            },
+             
+
+
+        ];
+        //JGrid.Bind();
+    }
+
+    function txtSearch_change() {
+
+
+        $("#JGrid").jsGrid("option", "pageIndex", 1);
+
+
+        if (txtSearch.value != "") {
+
+            let search: string = txtSearch.value.toLowerCase();
+            let SearchDetails = Display.filter(x => x.ID.toString().search(search) >= 0 || x.Title.toLowerCase().search(search) >= 0
+                || x.Type.toLowerCase().search(search) >= 0 || x.Remars.toLowerCase().search(search) >= 0);
+
+            JGrid.DataSource = SearchDetails;
+            JGrid.Bind();
+        } else {
+            JGrid.DataSource = Display;
+            JGrid.Bind();
+        }
+
+    }
+    function DisplayAll() {
+
+        Ajax.CallAsync({
+            url: Url.Action("Get_Data", "Profile"),
+            data: { Name_txt: "Catch_Receipt" },
+            success: (d) => {
+                let result = JSON.parse(d)
+
+                let res = result as Array<DataAll>;
+                 
+                Display_Grid(res)
+
+
+            }
+        })
+
+
+    }
+
+    function Display_Grid(_Display: Array<DataAll>) {
+        debugger
+        AllDisplay = _Display;
+        AllDisplay = AllDisplay.sort(dynamicSortNew("ID"));
+
+        Display = _Display;
+
+        //if (dbTypeF.value != "All") {
+        //    Display = Display.filter(x => x.Type == dbTypeF.value);
+        //}
+        //Display = Display.filter(x => x.TrDate >= txtDateFrom.value && x.TrDate <= txtDateTo.value);
+
+        Display = Display.sort(dynamicSortNew("ID"));
+        JGrid.DataSource = Display;
+        JGrid.Bind();
+
+        Clean();
+
+    }
 
     function AppTans(Type: string) {
 
@@ -69,8 +170,8 @@ namespace Money_Wallet {
         Model.Type = $('#TypeSours').val();
         Model.Title = Type;
 
-        //Model.ID = Number($('#txtTrNo').val());
-        Model.ID = 666;
+        Model.ID = Number($('#txtTrNo').val());
+        //Model.ID = 666;
 
         let Data = new Send_Data();
 
@@ -79,7 +180,7 @@ namespace Money_Wallet {
         Data.Model = JSON.stringify(Model);  
         Data.StatusFlag = 'u';
 
-        debugger
+        debugger            
         $.ajax({
             url: Url.Action("Add_Trans", "Profile"),
             type: "POST",
@@ -89,22 +190,62 @@ namespace Money_Wallet {
             success: (d) => {
                 let result = JSON.parse(d)
 
-                Clean();
                 let res = result as Array<DataAll>;
-
-
-                //Display_Grid(res)
-
-                //JGrid.SelectedItem = Model;
-                //GridDoubleClick();
+                Display_Grid(res)
+                Clean();
+                 
 
             }
         })
     }
 
+    function Delete(ID: number) {
+
+        JGrid.SelectedItem = Display.filter(x => x.ID == ID)[0];
+
+      
+
+        Model = new DataAll();
+         
+         
+        Model.ID = ID;
+
+        let Data = new Send_Data();
+
+        Data.ID = Number($('#txtTrNo').val());
+        Data.Name_Txt_Master = "Catch_Receipt";
+        Data.Model = JSON.stringify(Model);
+        Data.StatusFlag = 'd';
+
+        debugger
+        $.ajax({
+            url: Url.Action("Update_Data", "Profile"),
+            type: "POST",
+            dataType: 'json',
+            async: false,
+            data: { Data: JSON.stringify(Data) },
+            success: (d) => {
+                let result = JSON.parse(d)
+
+                let res = result as Array<DataAll>;
+                Display_Grid(res)
+                Clean();
+
+            }
+        })
+
+
+    }
+
 
     function Clean() {
-        $('#TypeSours').val('0'); 
+
+        debugger
+        let MaxID = AllDisplay[0].ID;
+        $('#txtTrNo').val(MaxID + 1);
+        $('#TrNoLab').html('TrNo ( ' + MaxID + 1+' )');
+
+        $('#TypeSours').val('Cash');
         $('#txtRemark').val('');
         $('#txtdate').val(GetDate());
         $('#txtAmount').val('');
@@ -113,7 +254,7 @@ namespace Money_Wallet {
         $('#btnReceipt').addClass('display_none');
         $('#btnExchange').addClass('display_none');
 
-        if (Glopl_Type == '1') {
+        if (Glopl_Type == 'Exchange') {
             $('#btnExchange').removeClass('display_none');
         }
         else {
